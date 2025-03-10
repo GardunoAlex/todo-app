f6efd8406c496ec4b9f96f16e57f28b9327e23d2
GardunoAlex <monopolyalex80@gmail.com>
2025-03-10 17:24:07 -0500

    ignore

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import env from 'dotenv';
 import pool from './config/db';
 
-dotenv.config();
+env.config();
 
 const app = express();
 app.use(cors());
